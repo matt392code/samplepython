360567d0a1ebcddf2f69e000734325bf0a072eca
matt392code <matt392@earthlink.net>
2015-09-16 21:36:34 -0400

    Determines the number of characters in a string.

---
+++ LenFunction.py
@@ -0,0 +1,9 @@
+print ("This program will determine the number of characters entered.")
+
+StringEntered = input("Enter a word or sentence; spaces are ok: ")
+print("The word or sentence that was entered was: ", StringEntered)
+
+NumberOfCharacters = len(StringEntered)
+
+print ("The number of characters in the string that was entered is: ", 
+       NumberOfCharacters)
